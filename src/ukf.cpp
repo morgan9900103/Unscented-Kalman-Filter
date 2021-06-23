e9c90653f7366fb3dd63c6491723e3257fe6f0ef
Morgan Lee <menghan.lee@ufl.edu>
2021-06-22 23:13:39 -0400

    Fixed bugs

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,7 +24,7 @@ UKF::UKF() {
     std_a_ = 3;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.5*M_PI;
+    std_yawdd_ = 2;
 
     /**
     * DO NOT MODIFY measurement noise values below.
@@ -67,11 +67,11 @@ UKF::UKF() {
     weights_.fill(0.5/(lambda_+n_aug_));
     weights_(0) = lambda_/(lambda_+n_aug_);
 
-    Rlidar_ = MatrixXd::Identity(2, 2);
+    Rlidar_ = MatrixXd(2, 2);
     Rlidar_(0, 0) = std_laspx_*std_laspx_;
-    Rlidar_(0, 0) = std_laspy_*std_laspy_;
+    Rlidar_(1, 1) = std_laspy_*std_laspy_;
 
-    Rradar_ = MatrixXd::Identity(3, 3);
+    Rradar_ = MatrixXd(3, 3);
     Rradar_(0, 0) = std_radr_*std_radr_;
     Rradar_(1, 1) = std_radphi_*std_radphi_;
     Rradar_(2, 2) = std_radrd_*std_radrd_;
@@ -91,6 +91,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
                   0,
                   0,
                   0;
+
+            P_.fill(0.0);
             P_(0, 0) = std_laspx_*std_laspx_;
             P_(1, 1) = std_laspy_*std_laspy_;
             P_(2, 2) = 1;
@@ -106,6 +108,8 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
             double p_y = rho * sin(phi);
 
             x_ << p_x, p_y, rho_dot, phi, 0;
+
+            P_.fill(0.0);
             P_(0, 0) = std_radr_*std_radr_;
             P_(1, 1) = std_radphi_*std_radphi_;
             P_(2, 2) = std_radrd_*std_radrd_;
@@ -137,8 +141,8 @@ void UKF::Prediction(double delta_t) {
     // Augment state and covariance matrix
     VectorXd x_aug = VectorXd(n_aug_);
     x_aug.head(5) = x_;
-    x_aug(5) = std_a_;
-    x_aug(6) = std_yawdd_;
+    x_aug(5) = 0;
+    x_aug(6) = 0;
 
     MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
     P_aug.fill(0.0);
@@ -157,53 +161,55 @@ void UKF::Prediction(double delta_t) {
     }
 
     // Sigma points prediction
-    for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-        double px = Xsig_aug(0, i);
-        double py = Xsig_aug(1, i);
+    for (int i = 0; i < 2 * n_aug_ + 1; ++i) {
+        // extract values for better readability
+        double p_x = Xsig_aug(0, i);
+        double p_y = Xsig_aug(1, i);
         double v = Xsig_aug(2, i);
-        double psi = Xsig_aug(3, i);
-        double psi_dot = Xsig_aug(4, i);
-        double mu_a = Xsig_aug(5, i);
-        double mu_psi_dd = Xsig_aug(6, i);
+        double yaw = Xsig_aug(3, i);
+        double yawd = Xsig_aug(4, i);
+        double nu_a = Xsig_aug(5, i);
+        double nu_yawdd = Xsig_aug(6, i);
 
-        double px_pred;
-        double py_pred;
-        if (fabs(psi_dot) < 0.0001) {
-            px_pred = v * cos(psi) * delta_t;
-            py_pred = v * sin(psi) * delta_t;
-        }
-        else {
-            px_pred = v/psi_dot*( sin(psi + psi_dot*delta_t) - sin(psi));
-            py_pred = v/psi_dot*(-cos(psi + psi_dot*delta_t) + cos(psi));
+        // predicted state values
+        double px_p, py_p;
+
+        // avoid division by zero
+        if (fabs(yawd) > 0.001) {
+            px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+            py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+        } else {
+            px_p = p_x + v*delta_t*cos(yaw);
+            py_p = p_y + v*delta_t*sin(yaw);
         }
 
-        Xsig_pred_(0, i) = px + px_pred + 0.5 * delta_t * delta_t * cos(psi) * mu_a;
-        Xsig_pred_(1, i) = py + py_pred + 0.5 * delta_t * delta_t * sin(psi) * mu_a;
-        Xsig_pred_(2, i) = v + delta_t * mu_a;
-        Xsig_pred_(3, i) = psi + psi_dot*delta_t + 0.5*delta_t*delta_t*mu_psi_dd;
-        Xsig_pred_(4, i) = psi_dot + delta_t*mu_psi_dd;
-    }
+        double v_p = v;
+        double yaw_p = yaw + yawd*delta_t;
+        double yawd_p = yawd;
+
+        // add noise
+        px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+        py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+        v_p = v_p + nu_a*delta_t;
+
+        yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+        yawd_p = yawd_p + nu_yawdd*delta_t;
+
+        // write predicted sigma point into right column
+        Xsig_pred_(0, i) = px_p;
+        Xsig_pred_(1, i) = py_p;
+        Xsig_pred_(2, i) = v_p;
+        Xsig_pred_(3, i) = yaw_p;
+        Xsig_pred_(4, i) = yawd_p;
+      }
+
 
     // Predict mean and covariance
-    /*VectorXd x = VectorXd(n_x_);
-    for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-        x += weights_(i)*Xsig_pred_.col(i);
-    }
-    x_ = x;*/
     x_.fill(0.0);
     for (int i = 0; i < 2 * n_aug_ + 1; i++) {
         x_ += weights_(i)*Xsig_pred_.col(i);
     }
 
-    /*MatrixXd P = MatrixXd(n_x_, n_x_);
-    P.fill(0.0);
-    for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-        VectorXd x_diff = Xsig_pred_.col(i) - x_;
-        while (x_diff(3) > M_PI) x_diff(3) -= 2. * M_PI;
-        while (x_diff(3) < -M_PI) x_diff(3) += 2. * M_PI;
-        P += weights_(i)*x_diff*x_diff.transpose();
-    }
-    P_ = P;*/
     P_.fill(0.0);
     for (int i = 0; i < 2 * n_aug_ + 1; i++) {
         VectorXd x_diff = Xsig_pred_.col(i) - x_;
@@ -287,23 +293,25 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
          meas_package.raw_measurements_(2);
 
     // Transfer predicted state into measurement state space
-    MatrixXd Zsig = MatrixXd(n_z, n_z);
-    for (int i = 0; i < 2 * n_aug_ + 1; i++) {
-        double px = Xsig_pred_(0, i);
-        double py = Xsig_pred_(1, i);
-        double v = Xsig_pred_(2, i);
-        double psi = Xsig_pred_(3, i);
-        double psi_dot = Xsig_pred_(4, i);
+    MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug_ + 1);
+    for (int i = 0; i < 2 * n_aug_ + 1; ++i) {  // 2n+1 simga points
+      // extract values for better readability
+      double p_x = Xsig_pred_(0, i);
+      double p_y = Xsig_pred_(1, i);
+      double v  = Xsig_pred_(2, i);
+      double yaw = Xsig_pred_(3, i);
 
-        double c1 = sqrt(px*px + py*py);
-        double c2 = px*cos(psi)*v + py*sin(psi)*v;
+      double v1 = cos(yaw)*v;
+      double v2 = sin(yaw)*v;
 
-        Zsig(0, i) = c1;
-        Zsig(1, i) = atan(py/px);
-        Zsig(2, i) = c2/c1;
+      // measurement model
+      Zsig(0, i) = sqrt(p_x*p_x + p_y*p_y);                     // r
+      Zsig(1, i) = atan2(p_y, p_x);                             // phi
+      Zsig(2, i) = (p_x*v1 + p_y*v2) / sqrt(p_x*p_x + p_y*p_y); // r_dot
     }
 
     VectorXd z_pred = VectorXd(n_z);
+    z_pred.fill(0.0);
     for (int i = 0; i < 2 * n_aug_ + 1; i++) {
         z_pred += weights_(i)*Zsig.col(i);
     }
@@ -315,7 +323,7 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
         VectorXd z_diff = Zsig.col(i) - z_pred;
         while (z_diff(1) > M_PI) z_diff(1) -= 2 * M_PI;
         while (z_diff(1) < -M_PI) z_diff(1) += 2 * M_PI;
-        S += weights_(i)*(z_diff)*(z_diff).transpose();
+        S += weights_(i)*z_diff*z_diff.transpose();
     }
 
     S += Rradar_;
@@ -339,6 +347,9 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
     MatrixXd K = Tc*S.inverse();
 
     // Update state
-    x_ += K*(z - z_pred);
+    VectorXd z_diff = z - z_pred;
+    while (z_diff(1) > M_PI) z_diff(1) -= 2 * M_PI;
+    while (z_diff(1) < -M_PI) z_diff(1) += 2 * M_PI;
+    x_ += K*z_diff;
     P_ -= K*S*K.transpose();
 }
