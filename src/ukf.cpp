d8608027d40daf5ea73fcd033fc94ddda89504e8
Morgan Lee <menghan.lee@ufl.edu>
2021-06-28 13:14:12 -0400

    Tuned parameters

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -21,10 +21,10 @@ UKF::UKF() {
     P_ = MatrixXd(5, 5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 3;
+    std_a_ = 2;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 2;
+    std_yawdd_ = 1;
 
     /**
     * DO NOT MODIFY measurement noise values below.
